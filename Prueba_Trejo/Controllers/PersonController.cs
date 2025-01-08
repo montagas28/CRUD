ad24779867e1db88609bfede7e73787d79a6ce4d
Programador Tic01 <bayron.trejo@montagas.com.co>
2025-01-08 00:49:47 -0500

    Initial commit

---
+++ Prueba_Trejo/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prueba_Trejo.Models;
+using Prueba_Trejo.Models.ViewPerson;
+
+namespace Prueba_Trejo.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            List<Person> lst = null;
+            using (Prueba_TrejoEntities db =  new Prueba_TrejoEntities())
+            {
+                lst=(from d in db.person
+                    select new Person
+                    {
+                        name=d.name,
+                        last_name=d.last_name,
+                        code=d.code,
+                        gender=d.gender,
+                        birth_date=d.birth_date,
+                        active=d.active,
+                    }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ Prueba_Trejo/Models/ViewPerson/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba_Trejo.Models.ViewPerson
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string code { get; set; }
+        public string gender { get; set; }
+        public Nullable<System.DateTime> birth_date { get; set; }
+        public Nullable<byte> active { get; set; }
+    }
+}
